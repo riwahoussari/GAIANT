198e9e51868f7b09f455b6de42493853122ebe4c
riwahoussari <houssaririwa@gmail.com>
2025-10-26 18:13:07 +0200

    add stacking cards animation

--- src/pages/ArchePage/components/AccelerateImpact.tsx
+++ src/pages/ArchePage/components/AccelerateImpact.tsx
@@ -2,13 +2,29 @@ import { useRef } from "react";
 import { SlideUpAnim } from "../../../components/ui/Anims";
 import { BlurredLandscapeBg } from "../../../components/ui/Backgrounds";
 import { TitleBlock } from "../../../components/ui/Titles";
-import { useInView } from "motion/react";
+import {
+  motion as m,
+  MotionValue,
+  useInView,
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+} from "motion/react";
 import { ARCHE_PAGE_DATA } from "../../../lib/data";
+
+const CARDS = [{}, {}, {}, {}];
 
 export default function AccelerateImpact() {
   const sectionRef = useRef<HTMLDivElement>(null);
   const isInView = useInView(sectionRef, { once: true, margin: "-5%" });
 
+  const cardsContainerRef = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: cardsContainerRef,
+    offset: ["start start", "end end"],
+  });
+
+  useMotionValueEvent(scrollYProgress, "change", (val) => console.log(val))
   return (
     <section
       ref={sectionRef}
@@ -17,9 +33,12 @@ export default function AccelerateImpact() {
       {/* bg */}
       <BlurredLandscapeBg blur={"lg"} />
 
-      <div className="side-padding my-container py-[10px]">
+      <div
+        className="side-padding my-container py-[10px]"
+        style={{ paddingBottom: `${CARDS.length * 50}px` }}
+      >
         {/* title */}
-        <div className="relative max-w-[520px] space-y-3 sm:space-y-5">
+        <div className="relative mb-12 max-w-[520px] space-y-3 sm:space-y-5">
           <TitleBlock title={ARCHE_PAGE_DATA.ACCELERATE_IMPACT.title} />
           <SlideUpAnim isInView={isInView} transition={{ delay: 0.2 }}>
             <p className="text-16">{ARCHE_PAGE_DATA.ACCELERATE_IMPACT.text}</p>
@@ -27,40 +46,72 @@ export default function AccelerateImpact() {
         </div>
 
         {/* card */}
-        <div className="bg-linear-white-transparent-70 mt-12 flex items-center justify-between gap-x-5 gap-y-10 rounded-xl px-4 py-10 backdrop-blur-[100px] max-lg:flex-col sm:mt-16 sm:gap-y-14 sm:px-10 sm:py-16 xl:p-20">
-          {/* text */}
-          <div className="max-w-[380px] text-white max-lg:text-center">
-            <SlideUpAnim isInView={isInView} transition={{ delay: 0.3 }}>
-              <p className="font-ibm! text-[11px] xs:text-[12px]">
-                {ARCHE_PAGE_DATA.ACCELERATE_IMPACT.cards[0].subtitle}
-              </p>
-            </SlideUpAnim>
-            <SlideUpAnim isInView={isInView} transition={{ delay: 0.4 }}>
-              <p className="text-[50px] xs:text-[60px]">
-                {" "}
-                {ARCHE_PAGE_DATA.ACCELERATE_IMPACT.cards[0].title}
-              </p>
-            </SlideUpAnim>
-            <SlideUpAnim isInView={isInView} transition={{ delay: 0.5 }}>
-              <p className="text-16">
-                {ARCHE_PAGE_DATA.ACCELERATE_IMPACT.cards[0].text}
-              </p>
-            </SlideUpAnim>
-          </div>
-          {/* image */}
-          <SlideUpAnim
-            className="w-full max-w-[700px] min-w-[320px] lg:w-6/10"
-            isInView={isInView}
-            transition={{ delay: 0.4 }}
-          >
-            <img
-              src={ARCHE_PAGE_DATA.ACCELERATE_IMPACT.cards[0].img.src}
-              alt={ARCHE_PAGE_DATA.ACCELERATE_IMPACT.cards[0].img.alt}
-              className="h-full w-full object-contain"
-            />
-          </SlideUpAnim>
+        <div ref={cardsContainerRef}>
+          {CARDS.map((_, i) => (
+            <Card progress={scrollYProgress} index={i} isInView={isInView} />
+          ))}
         </div>
       </div>
     </section>
+  );
+}
+
+function Card({
+  isInView,
+  index,
+  progress,
+}: {
+  isInView: boolean;
+  progress: MotionValue<number>;
+  index: number;
+}) {
+  const scaleRange = [index * (1 / CARDS.length), 1];
+  const targetScale = 1 - (CARDS.length - index) * 0.01;
+  const scale = useTransform(progress, scaleRange, [1, targetScale]);
+
+  return (
+    <div
+      className="sticky top-20"
+      style={{ marginTop: `-${index == 0 ? 0 : 66}px` }}
+    >
+      <m.div
+        style={{ scale, top: `${index * 50}px` }}
+        className={
+          "bg-linear-white-transparent-70 relative flex items-center justify-between gap-x-5 gap-y-10 rounded-xl px-4 py-10 pb-14 backdrop-blur-[100px] max-lg:flex-col sm:gap-y-14 sm:px-10 sm:py-16 sm:pb-20 xl:p-20 xl:pb-24"
+        }
+      >
+        {/* text */}
+        <div className="max-w-[380px] text-white max-lg:text-center">
+          <SlideUpAnim isInView={isInView} transition={{ delay: 0.3 }}>
+            <p className="font-ibm! text-[11px] xs:text-[12px]">
+              {ARCHE_PAGE_DATA.ACCELERATE_IMPACT.cards[0].subtitle}
+            </p>
+          </SlideUpAnim>
+          <SlideUpAnim isInView={isInView} transition={{ delay: 0.4 }}>
+            <p className="text-[50px] xs:text-[60px]">
+              {" "}
+              {ARCHE_PAGE_DATA.ACCELERATE_IMPACT.cards[0].title}
+            </p>
+          </SlideUpAnim>
+          <SlideUpAnim isInView={isInView} transition={{ delay: 0.5 }}>
+            <p className="text-16">
+              {ARCHE_PAGE_DATA.ACCELERATE_IMPACT.cards[0].text}
+            </p>
+          </SlideUpAnim>
+        </div>
+        {/* image */}
+        <SlideUpAnim
+          className="w-full max-w-[700px] min-w-[320px] lg:w-6/10"
+          isInView={isInView}
+          transition={{ delay: 0.4 }}
+        >
+          <img
+            src={ARCHE_PAGE_DATA.ACCELERATE_IMPACT.cards[0].img.src}
+            alt={ARCHE_PAGE_DATA.ACCELERATE_IMPACT.cards[0].img.alt}
+            className="h-full w-full object-contain"
+          />
+        </SlideUpAnim>
+      </m.div>
+    </div>
   );
 }
