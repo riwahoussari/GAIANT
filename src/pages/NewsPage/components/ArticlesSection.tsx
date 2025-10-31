3d46199a9896d748535bb4675a805d2d74a331fb
riwahoussari <houssaririwa@gmail.com>
2025-10-31 22:12:25 +0200

    fix spacing in news page

--- src/pages/NewsPage/components/ArticlesSection.tsx
+++ src/pages/NewsPage/components/ArticlesSection.tsx
@@ -33,7 +33,7 @@ export default function ArticlesSection({
     >
       {/* main article */}
       <div className="relative" ref={sectionRef}>
-        <p className="mb-5 font-ibm! text-[16px] xs:text-[18px]">
+        <p className="mb-10 font-ibm! text-[16px] xs:text-[18px]">
           <AnimatedText isInView={isInView}>THE GAIANT BLOG</AnimatedText>
         </p>
         <div
@@ -66,7 +66,7 @@ export default function ArticlesSection({
             {withBall && (
               <SlideUpAnim
                 isInView={isInView}
-                className="absolute w-1/3 -translate-x-1/4 translate-y-1/2 max-lg:bottom-0 max-lg:left-0 max-lg:opacity-90 lg:top-0 lg:right-0 lg:translate-x-1/2 lg:-translate-y-1/2"
+                className="absolute w-1/3 -translate-x-1/4 translate-y-1/2 max-lg:bottom-0 max-lg:left-0 max-lg:opacity-90 lg:top-0 lg:right-0 lg:translate-x-1/2 lg:-translate-y-1/3"
               >
                 <GradientCircle
                   className="blur-[max(2.5vw,25px)]!"
@@ -95,7 +95,7 @@ export default function ArticlesSection({
             <div className="mt-5 xs:mt-[30px] xl:mt-[44px]">
               <Button
                 arrow={"normal"}
-                className="gap-4! bg-transparent! pl-0! backdrop-blur-none!"
+                className="gap-4! bg-transparent! pl-0! backdrop-blur-[0px]!"
                 variant={"secondary"}
               >
                 READ FULL ARTICLE
@@ -106,7 +106,7 @@ export default function ArticlesSection({
       </div>
 
       {/* other articles */}
-      <div className="mt-10 grid grid-cols-1 gap-5 gap-y-10! xs:gap-8 sm:gap-5 lg:grid-cols-2 xl:grid-cols-3">
+      <div className="mt-11 grid grid-cols-1 gap-5 gap-y-10! xs:gap-8 sm:gap-5 lg:grid-cols-2 xl:grid-cols-3">
         {GENERAL_DATA.ARTICLES.slice(0, 3).map((article, i) => (
           <div>
             <SlideUpSelf className="w-full max-w-[550px] lg:max-w-none">
